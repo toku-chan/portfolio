4ad45f4becac3ad4a9977ee9256894a003b3abad
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-03-26 18:49:59 +0900

    chore: skillsコンポーネント作成

---
+++ src/components/Skills/Skills.tsx
@@ -0,0 +1,34 @@
+export function Skills() {
+  console.log("やれることってどういう指標で出せば良いかな・・・")
+
+  return (
+    <section>
+      <h1>スキル</h1>
+      {/* <p>とりあえずやったことがあることを書こう</p> */}
+      {/* <p>どんくらいできるかはまた後で</p> */}
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>ejs</li>
+        <li>sass</li>
+        <li>emotion</li>
+        <li>JavaScript</li>
+        <li>jQuery</li>
+        <li>TypeScript</li>
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>Vue.js</li>
+        <li>Nuxt.js</li>
+        <li>Apollo GraphQL</li>
+        <li>jest</li>
+        <li>testing-library</li>
+        <li>PHP</li>
+        <li>Laravel</li>
+        <li>Go</li>
+        <li>webpack</li>
+        <li>vite</li>
+        <li>gulp</li>
+      </ul>
+    </section>
+  )
+}
