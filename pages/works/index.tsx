925600352a4d2db06e054c12fd86b9cc238965a4
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-03-28 09:03:33 +0900

    chore: worksページを追加

---
+++ pages/works/index.tsx
@@ -0,0 +1,6 @@
+import type { NextPage } from 'next';
+import { WorksContent } from '~/feature/Works';
+
+const Works: NextPage = () => <WorksContent />
+
+export default Works
---
+++ src/feature/Works/index.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "~/components/Layout"
+
+export function WorksContent() {
+  return (
+    <Layout>
+      これまで作成してきたこと・携わってきたことを書く
+    </Layout>
+  )
+}
