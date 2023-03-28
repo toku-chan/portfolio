764e8b2ace38331d20afb62a2a2298a607c82168
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-03-28 09:11:59 +0900

    chore: Aboutの内容をベタ書きからobjectの形に変更

--- src/feature/Home/About/About.tsx
+++ src/feature/Home/About/About.tsx
@@ -1,14 +1,18 @@
-const myBirthDay = {
-  year: 1993,
-  month: 5,
-  date: 29
+const personalData = {
+  name: "中島一徳",
+  location: "千葉県柏市",
+  birthday: {
+    year: 1993,
+    month: 5,
+    date: 29
+  }
 }
 
 function getAge() {
   const today = new Date();
   const todayYear = today.getFullYear();
-  const thisYearBirthDay = new Date(todayYear, myBirthDay.month - 1, myBirthDay.date);
-  const age = todayYear - myBirthDay.year;
+  const thisYearBirthDay = new Date(todayYear, personalData.birthday.month - 1, personalData.birthday.date);
+  const age = todayYear - personalData.birthday.year;
 
   if(today < thisYearBirthDay) {
     return age - 1;
@@ -25,20 +29,16 @@ export function About() {
       <h1>About</h1>
       <div>
         <h2>名前</h2>
-        <p>中島一徳<span>なかじまいっとく</span></p>
+        <p>{personalData.name}</p>
       </div>
       <div>
         <h2>生年月日</h2>
-        <p>1993/05/29</p>
+        <p>{personalData.birthday.year}/{personalData.birthday.month}/{personalData.birthday.date}</p>
         <p>{age}歳</p>
       </div>
       <div>
-        <h2>性別</h2>
-        <p>男</p>
-      </div>
-      <div>
         <h2>現在住んでいるところ</h2>
-        <p>千葉県柏市</p>
+        <p>{personalData.location}</p>
       </div>
     </section>
   )
