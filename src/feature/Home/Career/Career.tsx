f5bc61fb2108333eeabc259af3d4656141b83487
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-03-28 09:19:55 +0900

    chore: careerの内容をデータから展開する形に変更

--- src/feature/Home/Career/Career.tsx
+++ src/feature/Home/Career/Career.tsx
@@ -1,3 +1,25 @@
+// TODO: いつノジマを退職したか記載
+// TODO: いつY'sの内定をもらったか記載
+// TODO: Y'sでの研修期間を記載
+const career = [
+  {
+    date: "2017/03",
+    detail: "日本大学商学部経営学科を卒業",
+  },
+  {
+    date: "2017/04",
+    detail: "株式会社ノジマに入社",
+  },
+  {
+    date: "2019/05",
+    detail: "株式会社Y'sに入社",
+  },
+  {
+    date: "2019/05",
+    detail: "合同会社EXNOA（旧: DMM GAMES）に常駐で勤務",
+  },
+];
+
 export function Career() {
   console.log("Careerはざっくりとした経歴を箇条書きで書いておく感じが良いかな？")
 
@@ -5,13 +27,10 @@ export function Career() {
     <section>
       <h1>Career</h1>
       <ul>
-        <li>2017/03: 日本大学商学部経営学科を卒業</li>
-        <li>2017/04: 株式会社ノジマに入社</li>
-        {/* <li>いつ退職したのかも記載しておきたい</li> */}
-        {/* <li>いつ内定をもらい、研修期間を記載したい</li> */}
-        <li>2019/05: 株式会社{`Y's`}に入社</li>
-        <li>2019/05: 合同会社EXNOA（旧: DMM GAMES）に常駐で勤務</li>
-        {/* <li>現場でどういうことをやったのか、別記載するのはありかも</li> → worksというページを作成した */}
+        {career.map((item) => (
+          // TODO: idとかにしておきたいが、暫定でdetailにしておく
+          <li key={item.detail}>{item.date}: {item.detail}</li>
+        ))}
       </ul>
     </section>
   )
