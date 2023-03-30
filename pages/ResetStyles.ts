3c44526e9ca7d5d742529a49241839200d61168f
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-03-30 09:37:24 +0900

    chore: set reset styles

---
+++ pages/ResetStyles.ts
@@ -0,0 +1,9 @@
+import { css } from "@emotion/react"
+
+export const ResetStyles = css`
+  html, body, * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app'
+import { Global } from '@emotion/react'
+import { ResetStyles } from './ResetStyles'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global styles={ResetStyles} />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
