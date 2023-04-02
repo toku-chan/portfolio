83a81b75b40f1a39fc9beacbf4fa2e2e9dfc3b77
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-04-02 17:38:33 +0900

    chore: スキル内容をベタ書きからconfigで配列データに変更

--- src/feature/Home/Skills/Skills.tsx
+++ src/feature/Home/Skills/Skills.tsx
@@ -1,5 +1,6 @@
 import styled from "@emotion/styled";
 import { Heading_1 } from "~/designSystems"
+import { mySkillSets } from "./config";
 
 // TODO: 他の場所もこのサイズ感にするが共通化するべきかは検討する
 const Content = styled.section`
@@ -12,30 +13,10 @@ export function Skills() {
   return (
     <Content>
       <Heading_1>スキル</Heading_1>
-      {/* <p>とりあえずやったことがあることを書こう</p> */}
-      {/* <p>どんくらいできるかはまた後で</p> */}
       <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>ejs</li>
-        <li>sass</li>
-        <li>emotion</li>
-        <li>JavaScript</li>
-        <li>jQuery</li>
-        <li>TypeScript</li>
-        <li>React.js</li>
-        <li>Next.js</li>
-        <li>Vue.js</li>
-        <li>Nuxt.js</li>
-        <li>Apollo GraphQL</li>
-        <li>jest</li>
-        <li>testing-library</li>
-        <li>PHP</li>
-        <li>Laravel</li>
-        <li>Go</li>
-        <li>webpack</li>
-        <li>vite</li>
-        <li>gulp</li>
+        {mySkillSets.map((item) => (
+          <li key={item.skill}>{item.skill}</li>
+        ))}
       </ul>
     </Content>
   )
