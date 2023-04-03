9d4ef7f8eaec481a5b356e745461ecb05961f176
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-04-03 23:00:58 +0900

    chore: footerにスタイルを当てる

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,21 @@
+import styled from '@emotion/styled';
+
+const FooterStyle = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-top: 36px;
+  padding: 18px 0;
+`;
+
+const Copyright = styled.div`
+  font-size: 11px;
+`;
+
 export function Footer() {
-  return <footer>コピーライトを書いておく</footer>;
+  return (
+    <FooterStyle>
+      <Copyright>コピーライト</Copyright>
+    </FooterStyle>
+  );
 }
