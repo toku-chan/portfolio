cc50fec9aaddc5fbbebebf332319630e3c52792a
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-04-03 23:00:40 +0900

    chore: reset styleに追加

--- pages/ResetStyles.ts
+++ pages/ResetStyles.ts
@@ -1,9 +1,16 @@
-import { css } from "@emotion/react"
+import { css } from '@emotion/react';
 
 export const ResetStyles = css`
-  html, body, * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  html,
+  body {
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    ul li {
+      list-style: none;
+    }
   }
 `;
