44481f5f3b55fc95a7aabeb77ed5a0ac44528b7c
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-04-03 22:37:39 +0900

    chore: interestのContent指定忘れを対応

--- src/feature/Interest/Detail/Camping.tsx
+++ src/feature/Interest/Detail/Camping.tsx
@@ -10,13 +10,13 @@ const Content = styled.section`
 export function Camping() {
   // MEMO: ブログ作ったらそっちに誘導するとかでも良いかも
   return (
-    <section>
+    <Content>
       <Heading_2>キャンプ</Heading_2>
       <p>
         昔やっていたけど、ずっとできていないのでやりたい。
         <br />
         在宅でずっと家だから、無になりにいきたい。焚き火を一生眺めていたい。
       </p>
-    </section>
+    </Content>
   );
 }
