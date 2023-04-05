07c01c93c004c1d155a9505bb981ade5e19a7798
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-04-05 09:13:06 +0900

    chore: worksのstyleを調整

--- src/feature/Works/Ys/index.tsx
+++ src/feature/Works/Ys/index.tsx
@@ -2,15 +2,13 @@ import styled from '@emotion/styled';
 import { Heading_2 } from '~/designSystems';
 import { simulatedProductsList } from './config';
 
-// TODO: 他の場所もこのサイズ感にするが共通化するべきかは検討する
-const Content = styled.section`
-  margin: 36px auto 0;
-  padding: 0;
+const DetailWrapper = styled.section`
+  margin-top: 18px;
 `;
 
 export function Ys() {
   return (
-    <Content>
+    <DetailWrapper>
       <Heading_2>株式会社{`Y's`}</Heading_2>
       <ul>
         {simulatedProductsList.map(item => (
@@ -20,6 +18,6 @@ export function Ys() {
           </li>
         ))}
       </ul>
-    </Content>
+    </DetailWrapper>
   );
 }
--- src/feature/Works/index.tsx
+++ src/feature/Works/index.tsx
@@ -1,19 +1,25 @@
-import Link from 'next/link';
+import styled from '@emotion/styled';
 import { Layout } from '~/components/Layout';
 import { Heading_1 } from '~/designSystems';
 import { Exnoa } from './EXNOA';
 import { Ys } from './Ys';
 
+const Content = styled.section`
+  margin: 36px auto 0;
+  padding: 0;
+`;
+
 export function WorksContent() {
   return (
     <Layout>
-      <Heading_1>制作実績</Heading_1>
-      <p>
-        今までやってきたことを記載しておく。実績見せれないので、とりあえずやったことの模擬実装とかはアリかも
-      </p>
-      <Exnoa />
-      <Ys />
-      <Link href='/'>Back To Home</Link>
+      <Content>
+        <Heading_1>制作実績</Heading_1>
+        <p>
+          今までやってきたことを記載しておく。実績見せれないので、とりあえずやったことの模擬実装とかはアリかも
+        </p>
+        <Exnoa />
+        <Ys />
+      </Content>
     </Layout>
   );
 }
