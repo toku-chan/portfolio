0f19777bc7eb031ceeb480f9ed67d1f2f706974b
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-04-09 16:45:48 +0900

    chore: github iconを追加

--- src/feature/Home/Personal/index.tsx
+++ src/feature/Home/Personal/index.tsx
@@ -1,4 +1,15 @@
 import Image from 'next/image';
+import styled from '@emotion/styled';
+
+const List = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const Item = styled.li`
+  margin-right: 8px;
+`;
 
 export function Personal() {
   return (
@@ -7,10 +18,13 @@ export function Personal() {
         <li>中島一徳</li>
         <li>{`Y's`}</li>
         <li>
-          <span>SNS</span>
-          <ul>
-            <li>
-              <a href=''>
+          <List>
+            <Item>
+              <a
+                href='https://twitter.com/web_ittokun'
+                target='_blank'
+                rel='noreferrer'
+              >
                 <Image
                   src='/sns/twitter.svg'
                   alt='Twitter'
@@ -18,9 +32,22 @@ export function Personal() {
                   height='32'
                 />
               </a>
-            </li>
-            <li>GitHub</li>
-          </ul>
+            </Item>
+            <Item>
+              <a
+                href='https://github.com/toku-chan'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <Image
+                  src='/sns/github.svg'
+                  alt='GitHub'
+                  width='32'
+                  height='32'
+                />
+              </a>
+            </Item>
+          </List>
         </li>
       </ul>
     </div>
