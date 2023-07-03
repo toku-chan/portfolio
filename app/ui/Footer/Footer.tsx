30720002c908f003b03bcacbc78a4a59510b5a5a
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-07-03 09:34:57 +0900

    chore: app/ui/footerを修正

--- app/ui/Footer/Footer.tsx
+++ app/ui/Footer/Footer.tsx
@@ -3,14 +3,22 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+`;
+
 const CopyRight = styled.span`
   font-size: 12px;
 `;
 
 export const Footer: React.FC = () => {
   return (
-    <footer>
+    <StyledFooter>
       <CopyRight>©︎ i.nakajima</CopyRight>
-    </footer>
+    </StyledFooter>
   );
 };
