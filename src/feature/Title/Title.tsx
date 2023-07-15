a508de6116bb1a2d059d502164f8a14e724e89e7
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-07-15 11:12:19 +0900

    chore: 使用感を確認するためのテスト実装

--- src/feature/Title/Title.tsx
+++ src/feature/Title/Title.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { breakPoint } from '~/config';
 
 // NOTE: いつかロゴとかにしてみたい
 const Logo = styled.span`
   font-size: 40px;
   font-weight: bold;
+  color: blue;
+
+  // HACK: テスト実装のため、後日修正すること
+  ${breakPoint.mediaQueryPc} {
+    color: red;
+  }
 `;
 
 export const Title: React.FC = () => {
