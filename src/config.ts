e86f79e2dabb731d03f352bd75ec768ef0d1cce8
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-07-17 23:04:08 +0900

    chore: 共通設定を追加

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,24 @@
  * * feature共通のconfigを管理する
  */
 
-// NOTE: モバイルファーストで実装するため、breakPointはPc表示
 export const breakPoint = {
+  mediaQuerySp: '@media (max-width: 768px)',
   mediaQueryPc: '@media (min-width: 769px)',
 };
+
+/**
+ * NOTE
+ * * 以下の利用を目的に共通の数値を管理している（8の倍数）
+ * * font-size
+ * * margin
+ * * padding
+ */
+export const SpaceSize = {
+  base_x0_5: 8,
+  base: 16,
+  base_x1_5: 24,
+  base_x2: 32,
+  base_x2_5: 40,
+  base_x3: 48,
+  base_x4: 64,
+} as const;
