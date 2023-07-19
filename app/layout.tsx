72bd0744f3c86626ad539399a2045761c8e277d7
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-07-19 23:33:16 +0900

    chore: UAで取得するように変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { headers } from 'next/headers';
 import RestStyledComponent from './RestStyledComponent';
 import { Header } from '~/feature/Layout/Header';
 import { MainContent } from '~/feature/Layout/MainContent';
@@ -19,11 +20,29 @@ export const metadata: Metadata = {
   viewport: 'width=device-width, initial-scale=1',
 };
 
+// HACk: UAがいつ利用できなくなるかはわからないが、それまでは利用し続ける
+// TODO: contextで持ち、どこでも利用できるようにしたい
+function getDevice() {
+  const userAgent = headers().get('user-agent') as string;
+  const isIOS = userAgent.indexOf('iPhone') > 0;
+  const isAndroid =
+    userAgent.indexOf('Android') > 0 && userAgent.indexOf('Mobile') > 0;
+  const isSp = isIOS || isAndroid;
+
+  return {
+    isSp,
+    isIOS,
+    isAndroid,
+  };
+}
+
 export default function RootLayout({
   // Layouts must accept a children prop.
   // This will be populated with nested layouts or pages
   children,
 }: LayoutProps) {
+  const { isSp, isIOS, isAndroid } = getDevice();
+  console.log(isSp, isIOS, isAndroid);
   return (
     <html lang='ja'>
       <body>
