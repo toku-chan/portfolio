9c8bf3534e168c4feecd0d859579fbc3354d7707
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-07-23 13:03:18 +0900

    chore: 既存Productコンポーネントを修正

--- src/feature/Products/index.tsx
+++ src/feature/Products/index.tsx
@@ -1,16 +1,17 @@
 import styled from '@emotion/styled';
+import { SpaceSize } from '~/config';
 import { Exnoa } from './EXNOA';
 import { Ys } from './Ys';
 
-const Content = styled.section`
-  padding: 0;
+const Wrapper = styled.div`
+  padding-top: ${SpaceSize.base_x2}px;
 `;
 
 export function Products() {
   return (
-    <Content>
+    <Wrapper>
       <Exnoa />
       <Ys />
-    </Content>
+    </Wrapper>
   );
 }
