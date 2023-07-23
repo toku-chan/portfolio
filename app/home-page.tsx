2d5f3d9ae5d802fb181bd44f42932a28114ed72b
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-07-23 13:04:31 +0900

    chore: 大枠となるレイアウトをひとまず実装

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -3,11 +3,35 @@
 'use client';
 
 import { H1 } from '~/feature/Heading';
+import { Section } from '~/feature/Section';
+import { Introduction } from '~/feature/Introduction';
+import { Career } from '~/feature/Career';
+import { Skills } from '~/feature/Skills';
+import { Products } from '~/feature/Products';
+import { Contact } from '~/feature/Contact';
 
 export default function Home() {
+  // TODO: Aboutのコンポーネントを作成とかしていないが、全体を作ったときに煩雑に感じた場合は検討し直す
   return (
     <>
-      <H1>About</H1>
+      <Section>
+        {/* ICON */}
+        <H1>ABOUT</H1>
+        <Introduction />
+        <Career />
+      </Section>
+      <Section>
+        <H1>SKILL</H1>
+        <Skills />
+      </Section>
+      <Section>
+        <H1>PRODUCT</H1>
+        <Products />
+      </Section>
+      <Section>
+        <H1>CONTACT</H1>
+        <Contact />
+      </Section>
     </>
   );
 }
