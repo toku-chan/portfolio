6896289af244a7f286607f5a790cacc843ac4e73
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-09-03 10:58:00 +0900

    chore: Navigationにテストを追加

---
+++ src/app/(components)/Navigation.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import { Navigation } from './Navigation';
+
+// NOTE: テストがないことでActionsがこけるので、clientのNavigationコンポーネントで仮のテストを実行
+describe('Navigation', () => {
+  test('テスト｜リンクが表示されている', () => {
+    const { getByText } = render(<Navigation />);
+    expect(getByText('Home')).toBeInTheDocument();
+    expect(getByText('About')).toBeInTheDocument();
+    expect(getByText('Products')).toBeInTheDocument();
+    expect(getByText('Contact')).toBeInTheDocument();
+  });
+});
