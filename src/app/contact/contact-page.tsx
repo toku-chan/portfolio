acf49083f4a148bf3d0a0dce8922adf1effa83fc
nakajima-ittoku <nakajima.web.tips@gmail.com>
2023-09-03 10:19:37 +0900

    chore: Contactページの更新

--- src/app/contact/contact-page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { Contact } from '~/feature/Contact';
-
-export default function ContactPage() {
-  return <Contact />;
-}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,16 @@
-import ContactPage from './contact-page';
+import { Metadata } from 'next';
 
-export default function Page() {
-  return <ContactPage />;
+export const metadata: Metadata = {
+  title: '中島一徳 - Contact | PortFolio',
+  description: 'ご依頼はコンタクトページより',
+  viewport: 'width=device-width, initial-scale=1',
+};
+
+export default function Contact() {
+  return (
+    <section>
+      <h1>Contact</h1>
+      <p>依頼フォームを作成中</p>
+    </section>
+  );
 }
